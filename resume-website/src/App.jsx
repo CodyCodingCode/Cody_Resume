eebc3aa529e127bd6494ebacb98393076835fcc2
Cody Zhang <codyzhang4@hotmail.com>
2025-07-07 00:55:49 -0700

    comment out About section

--- resume-website/src/App.jsx
+++ resume-website/src/App.jsx
@@ -1,15 +1,15 @@
-// import Header from "./components/Header";
-// import About from "./components/AboutMe";
-// import Experience from "./components/Experience";
-// import Projects from "./components/Projects";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+// import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="font-sans text-gray-800 bg-white dark:bg-gray-900 dark:text-white min-h-screen">
       <Header />
       <main className="px-6 md:px-16 py-8 space-y-20">
-        <About />
+        {/* <About /> */}
         <Experience />
         <Projects />
       </main>
